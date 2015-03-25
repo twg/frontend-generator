21a415e76de52ce4da994f4db965b4baee39ab33
themarkappleby <themarkappleby@gmail.com>
2015-03-25 09:41:35 -0400

    moved questions to top of slushfile

--- slushfile.js
+++ slushfile.js
@@ -8,96 +8,99 @@
 
 'use strict';
 
-var gulp = require('gulp'),
-    install = require('gulp-install'),
-    conflict = require('gulp-conflict'),
-    template = require('gulp-template'),
-    rename = require('gulp-rename'),
-    path = require('path'),
-    _ = require('underscore.string'),
-    inquirer = require('inquirer');
+var path = require('path');
+var defaults = getDefaults();
+
+var questions = [
+  {
+    name: 'appName',
+    message: 'What is the name of your project?',
+    default: defaults.appName
+  },
+  {
+    name: 'appDescription',
+    message: 'How would you describe your project?',
+    default: 'Client Node application'
+  },
+  {
+    name: 'appIncludes',
+    message: 'Select includes:',
+    type: 'checkbox',
+    choices: [
+      {
+        name: 'TWG Frontend Scaffolding',
+        value: 'scaffolding',
+        checked: true
+      },
+      {
+        name: 'AngularJS',
+        value: 'angular',
+        checked: true
+      }
+    ]
+  }
+];
+
+// -------------------------------------
+
+var gulp = require('gulp');
+var install = require('gulp-install');
+var conflict = require('gulp-conflict');
+var template = require('gulp-template');
+var rename = require('gulp-rename');
+var _ = require('underscore.string');
+var inquirer = require('inquirer');
 
 function format(string) {
-    var username = string.toLowerCase();
-    return username.replace(/\s/g, '');
+  var username = string.toLowerCase();
+  return username.replace(/\s/g, '');
 }
 
-var defaults = (function () {
-    var workingDirName = path.basename(process.cwd()),
-      homeDir, osUserName, configFile, user;
+function getDefaults(){
+  var workingDirName = path.basename(process.cwd()),
+  homeDir, osUserName, configFile, user;
 
-    if (process.platform === 'win32') {
-        homeDir = process.env.USERPROFILE;
-        osUserName = process.env.USERNAME || path.basename(homeDir).toLowerCase();
-    }
-    else {
-        homeDir = process.env.HOME || process.env.HOMEPATH;
-        osUserName = homeDir && homeDir.split('/').pop() || 'root';
-    }
+  if (process.platform === 'win32') {
+    homeDir = process.env.USERPROFILE;
+    osUserName = process.env.USERNAME || path.basename(homeDir).toLowerCase();
+  }
+  else {
+    homeDir = process.env.HOME || process.env.HOMEPATH;
+    osUserName = homeDir && homeDir.split('/').pop() || 'root';
+  }
 
-    configFile = path.join(homeDir, '.gitconfig');
-    user = {};
+  configFile = path.join(homeDir, '.gitconfig');
+  user = {};
 
-    if (require('fs').existsSync(configFile)) {
-        user = require('iniparser').parseSync(configFile).user;
-    }
+  if (require('fs').existsSync(configFile)) {
+    user = require('iniparser').parseSync(configFile).user;
+  }
 
-    return {
-        appName: workingDirName,
-        userName: osUserName || format(user.name || ''),
-        authorName: user.name || '',
-        authorEmail: user.email || ''
-    };
-})();
+  return {
+    appName: workingDirName,
+    userName: osUserName || format(user.name || ''),
+    authorName: user.name || '',
+    authorEmail: user.email || ''
+  };
+}
 
 gulp.task('default', function (done) {
-    var prompts = [
-      {
-        name: 'appName',
-        message: 'What is the name of your project?',
-        default: defaults.appName
-      },
-      {
-        name: 'appDescription',
-        message: 'How would you describe your project?',
-        default: 'Client Node application'
-      },
-      {
-        name: 'appIncludes',
-        message: 'Select includes:',
-        type: 'checkbox',
-        choices: [
-          {
-            name: 'TWG Frontend Scaffolding',
-            value: 'scaffolding',
-            checked: true
-          },
-          {
-            name: 'AngularJS',
-            value: 'angular',
-            checked: true
+  inquirer.prompt(questions,
+    function (answers) {
+      answers.appNameSlug = _.slugify(answers.appName);
+      answers.appError = '<%= error.message %>';
+      gulp.src([__dirname + '/templates/**'], {dot: true})
+        .pipe(template(answers))
+        .pipe(rename(function (file) {
+          if (file.basename[0] === '_') {
+            file.basename = '.' + file.basename.slice(1);
           }
-        ]
-
-      }
-    ];
-    //Ask
-    inquirer.prompt(prompts,
-        function (answers) {
-            answers.appNameSlug = _.slugify(answers.appName);
-            answers.appError = '<%= error.message %>';
-            gulp.src([__dirname + '/templates/**'], {dot: true})
-                .pipe(template(answers))
-                .pipe(rename(function (file) {
-                    if (file.basename[0] === '_') {
-                        file.basename = '.' + file.basename.slice(1);
-                    }
-                }))
-                .pipe(conflict('./'))
-                .pipe(gulp.dest('./'))
-                .pipe(install())
-                .on('end', function () {
-                    done();
-                });
+        }))
+        .pipe(conflict('./'))
+        .pipe(gulp.dest('./'))
+        .pipe(install())
+        .on('end', function () {
+          done();
         });
+    });
 });
